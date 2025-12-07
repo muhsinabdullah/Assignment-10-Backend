be7f7a0ce3671a21a6ee3ae892ceb0bbaa298ce8
muhsinabdullah <muhsinabdullah1250@gmail.com>
2025-12-07 17:51:21 +0600

    Backend Structure setup

---
+++ index.js
@@ -0,0 +1,37 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+const port = 3000;
+
+const app = express();
+app.use(cors());
+
+
+const uri = "mongodb+srv://missionscic:mYL0htoojYFx9pHf@cluster0.a2ybfki.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req, res)=>{
+    res.send('hello developers');
+})
+app.listen(port, ()=>{
+    console.log(`sever is running on ${port}`);
+})
